d63d62bb901782a18b9cbe9d1505e56479ec1a75
Gin <gintamatama0912@gmail.com>
2022-06-08 15:05:44 +0800

    暴露配置

--- lib/cstlog.dart
+++ lib/cstlog.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 export 'package:cstlog/src/core/log.dart';
 export 'package:cstlog/src/model/log_event.dart';
 export 'package:cstlog/src/model/log_file_info.dart';
+export 'package:cstlog/src/core/config.dart';
 
 class Cstlog {
   static const MethodChannel _channel = MethodChannel('cstlog');
