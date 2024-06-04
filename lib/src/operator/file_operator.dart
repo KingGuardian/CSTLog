3c3402563a2d1abb5780e1f1d87204c902c42dda
Gin <gintamatama0912@gmail.com>
2024-06-04 13:02:42 +0800

    文件导出时增加重试机制

--- lib/src/operator/file_operator.dart
+++ lib/src/operator/file_operator.dart
@@ -13,7 +13,7 @@ class FileOperator implements Operator {
   Future<String> exportDevelopLogTo(
       String path, List<LogFileInfo> logList) async {
     String errorMessage = '';
-    String newLogPath = path + Platform.pathSeparator + additelLogFolderName;
+    final newLogPath = path + Platform.pathSeparator + additelLogFolderName;
     try {
       await _createFolderIfNotExist(newLogPath);
     } catch (error) {
@@ -22,12 +22,13 @@ class FileOperator implements Operator {
     }
     try {
       for (LogFileInfo logFileInfo in logList) {
-        File file = File.fromUri(logFileInfo.uri);
-        await file.copy(newLogPath +
-            Platform.pathSeparator +
-            logFileInfo.fileName +
-            '.' +
-            fileExtensionName);
+        await _copyWithRetry(
+          newLogPath: newLogPath,
+          fileName: logFileInfo.fileName,
+          oldfileUri: logFileInfo.uri,
+        );
+        // 为了避免文件复制失败，延迟一段时间
+        await Future.delayed(Duration(milliseconds: 100));
       }
     } catch (error) {
       errorMessage = error.toString();
@@ -39,7 +40,7 @@ class FileOperator implements Operator {
   Future<String> exportRecordTo(
       String path, List<RecordInfo> recordList) async {
     String errorMessage = '';
-    String newRecordPath =
+    final newRecordPath =
         path + Platform.pathSeparator + additelRecordFolderName;
     try {
       await _createFolderIfNotExist(newRecordPath);
@@ -52,19 +53,66 @@ class FileOperator implements Operator {
       for (RecordInfo recordInfo in recordList) {
         final uri = recordInfo.uri;
         if (uri != null) {
-          File file = File.fromUri(uri);
-          String fileName = recordInfo.fileName ?? recordInfo.title;
-          await file.copy(newRecordPath +
-              Platform.pathSeparator +
-              fileName +
-              '.' +
-              fileExtensionName);
+          await _copyWithRetry(
+            newLogPath: newRecordPath,
+            fileName: recordInfo.fileName ?? recordInfo.title,
+            oldfileUri: uri,
+          );
+          // 为了避免文件复制失败，延迟一段时间
+          await Future.delayed(Duration(milliseconds: 100));
+
+          // final file = File.fromUri(uri);
+          // String fileName = recordInfo.fileName ?? recordInfo.title;
+          // await file.copy(newRecordPath +
+          //     Platform.pathSeparator +
+          //     fileName +
+          //     '.' +
+          //     fileExtensionName);
         }
       }
     } catch (error) {
       errorMessage = error.toString();
     }
     return errorMessage;
+  }
+
+  Future<void> _copyWithRetry({
+    required String newLogPath,
+    required String fileName,
+    required Uri oldfileUri,
+  }) async {
+    final targetFilePath = newLogPath +
+        Platform.pathSeparator +
+        fileName +
+        '.' +
+        fileExtensionName;
+    final file = File.fromUri(oldfileUri);
+    int retryTimes = 5;
+    while (retryTimes > 0) {
+      try {
+        final targetFile = File(targetFilePath);
+        if (targetFile.existsSync()) {
+          targetFile.deleteSync();
+        }
+        final copiedFile = file.copySync(targetFilePath);
+        // 不确定是否真的复制成功，延迟一段时间再检查
+        Future.delayed(Duration(milliseconds: 100));
+        // 检查是否真的复制成功
+        if (copiedFile.existsSync()) {
+          final targetFileSize = copiedFile.lengthSync();
+          final sourceFileSize = file.lengthSync();
+          if (targetFileSize == sourceFileSize) {
+            break;
+          } else {
+            retryTimes--;
+          }
+        } else {
+          retryTimes--;
+        }
+      } catch (error) {
+        retryTimes--;
+      }
+    }
   }
 
   _createFolderIfNotExist(String path) async {
